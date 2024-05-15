7da95a660ebf81ddd95074c6ca5473d4f2284b2f
abelaba <se.abel.abate@gmail.com>
2024-05-15 11:39:58 +0300

    Add gemini pro vision for generating text from images

--- gemini.py
+++ gemini.py
@@ -1,16 +1,27 @@
 import sys
 import google.generativeai as genai
+import PIL.Image
 
 GOOGLE_API_KEY="" # Replace with google api key
 genai.configure(api_key=GOOGLE_API_KEY)
+
 model = genai.GenerativeModel('gemini-pro')
+image_model = genai.GenerativeModel('gemini-pro-vision')
 
 def main():
     arguments = sys.argv[1:]
-
     prompt = "".join(arguments)
+    prompt_list = prompt.split(" ")
 
-    response = model.generate_content(prompt)
+    if len(prompt_list) >= 1 and len(prompt_list) == 2 and prompt_list[0] == '--image':
+        img = PIL.Image.open(prompt_list[1])
+        response = image_model.generate_content(img)
+    elif len(prompt_list) >= 1 and len(prompt_list) > 2 and prompt_list[0] == '--image':
+        img = PIL.Image.open(prompt_list[1])
+        response = image_model.generate_content([''.join(prompt_list[2:]), img])
+    else:
+        prompt = "".join(arguments)
+        response = model.generate_content(prompt)
 
     print(":)")
     print()
