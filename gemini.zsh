f2c5ada639cbf2b6e643d113142d79cfeea45262
abelaba <se.abel.abate@gmail.com>
2024-05-26 01:27:14 +0300

    Update script command

--- gemini.zsh
+++ gemini.zsh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 if [ -z "$1" ]; then
   echo "Usage: $0 <text>"
@@ -6,25 +6,6 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-if [ "$1" == "--file" ]; then
-  if [ -z "$2" ]; then
-    echo "Usage: $0 --file <filename>"
-    echo ""
-    exit 1
-  fi
-
-  file="$2"
-
-  if [ ! -f "$file" ]; then
-    echo "File not found: $file"
-    exit 1
-  fi
-
-  text=$(cat "$file")
-else
-  text="$*"
-fi
-
-echo "$text"
+text="$*"
 
 python3 ./gemini.py "$text"
